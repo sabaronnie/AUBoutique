1bae1e3879397ac6c6f59515f2400a03302a2c82
Ronnie Saba <sabawronnie@gmail.com>
2024-10-28 01:09:36 +0200

    FIle Removals

---
+++ trialanderror.py
@@ -0,0 +1,20 @@
+import time
+import os
+def emptyTerminal():
+    if os.name == 'nt':  # For Windows
+        os.system('cls')
+    else:  # For Linux and macOS
+        os.system('clear')
+counter = 2
+while counter > 0:
+    print("Signing in", end= "", flush=True)
+    print(".", end="", flush=True)
+    time.sleep(0.5)
+    print(".", end="", flush=True)
+    time.sleep(0.5)
+    print(".", end="", flush=True)
+    time.sleep(0.5)
+    emptyTerminal()
+    counter -= 1
+
+
